13fedda678a70a8dbc16162a95fb45fc841da357
FX2One <dfoxik@gmail.com>
2021-01-06 17:56:48 +0100

    model serilizer

--- api/serializers.py
+++ api/serializers.py
@@ -2,18 +2,17 @@ from rest_framework import serializers
 from .models import Article
 
 #Serializer to serialize our Article class
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    author = serializers.CharField(max_length=100)
-    email = serializers.EmailField(max_length=100)
-    date = serializers.DateTimeField()
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = [
+            'id',
+            'title',
+            'author'
+        ]
 
-    def create(self, validated_data):
-        return Article.objects.create(validated_data)
 
-    def update(self, instance, validated_data):
-        instance.title = validate_data.get('title', instance.title)
-        instance.author = validate_data.get('title', instance.author)
-        instance.email = validate_data.get('title', instance.email)
-        instance.date = validate_data.get('title', instance.date)
+
+
+
 
