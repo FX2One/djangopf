160162d8f393fcd97586dd43e44544783a65e8f3
FX2One <dfoxik@gmail.com>
2021-01-08 13:23:25 +0100

    ViewSet and Routers

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 #from .views import article_list, article_detail
-from .views import ArticleAPIView, ArticleDetails, GenericAPIView
+from .views import ArticleAPIView, ArticleDetails, GenericAPIView, ArticleViewSet
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'article',ArticleViewSet, basename="Article View Set")
 
 #created news URLS for our article_list VIEW
 urlpatterns = [
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>/', include(router.urls)),
     #path('article/', article_list),
     #path('detail/<int:pk>', article_detail),
     path('article/', ArticleAPIView.as_view()),
